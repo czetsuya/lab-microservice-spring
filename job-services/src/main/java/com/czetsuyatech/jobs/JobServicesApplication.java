d89ea71684abf686b8c3f1753ab6f80b81c31555
Edward P. Legaspi <czetsuya@gmail.com>
2021-12-06 19:03:40 +0800

    Initial commit

---
+++ job-services/src/main/java/com/czetsuyatech/jobs/JobServicesApplication.java
@@ -0,0 +1,13 @@
+package com.czetsuyatech.jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobServicesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JobServicesApplication.class, args);
+  }
+
+}
---
+++ job-services/src/test/java/com/czetsuyatech/jobs/JobServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czetsuyatech.jobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobServicesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
