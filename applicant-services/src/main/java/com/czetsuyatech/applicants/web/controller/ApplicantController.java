05b20278117fc11605d0b9f59fc2331491e664f2
Edward P. Legaspi <czetsuya@gmail.com>
2021-12-08 19:41:43 +0800

    feat: circuit breaker

--- applicant-services/src/main/java/com/czetsuyatech/applicants/web/controller/ApplicantController.java
+++ applicant-services/src/main/java/com/czetsuyatech/applicants/web/controller/ApplicantController.java
@@ -1,5 +1,8 @@
 package com.czetsuyatech.applicants.web.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
@@ -15,5 +18,18 @@ public class ApplicantController {
 
     log.debug("get applicants by job");
     return Arrays.asList("Steve", "Bill", "Linus");
+  }
+
+  @Retry(name = "default", fallbackMethod = "getTopApplicantsByJoDefault")
+  @CircuitBreaker(name = "default", fallbackMethod = "getTopApplicantsByJoDefault")
+  @GetMapping("top-applicants-by-job")
+  public List<String> getTopApplicantsByJo() {
+
+    log.debug("get top applicants by job");
+    return null;
+  }
+
+  public List<String> getTopApplicantsByJoDefault() {
+    return new ArrayList<>();
   }
 }
